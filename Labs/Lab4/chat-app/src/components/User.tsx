34d7967ddbca71fe603782d947ba3b9e9df27abf
Logan <lpl29@uclive.ac.nz>
2022-05-08 15:41:59 +1200

    More lab stuff and added reference server to assignmnet 2

---
+++ Labs/Lab4/chat-app/src/components/User.tsx
@@ -0,0 +1,89 @@
+import {Link, useNavigate, useParams} from "react-router-dom";
+import React from "react";
+import axios from "axios";
+
+const User = () => {
+    const {id} = useParams()
+    const [user, setUser] = React.useState<User>({user_id:0, username:""})
+    const [errorFlag, setErrorFlag] = React.useState(false)
+    const [errorMessage, setErrorMessage] = React.useState("")
+    const navigate = useNavigate();
+
+    const deleteUser = (user: User) => {
+        axios.delete('http://localhost:3000/api/users/' + user.user_id)
+            .then((response) => {
+                navigate('/users')
+            }, (error) => {
+                setErrorFlag(true)
+                setErrorMessage(error.toString())
+            })
+    }
+
+    React.useEffect(() => {
+        const getUser = () => {
+            axios.get('http://localhost:3000/api/users/'+id)
+                .then((response) => {
+                    setErrorFlag(false)
+                    setErrorMessage("")
+                    setUser(response.data)
+                }, (error) => {
+                    setErrorFlag(true)
+                    setErrorMessage(error.toString())
+                })
+        }
+        getUser()
+    }, [id])
+
+    if (errorFlag) {
+        return (
+            <div>
+                <h1>User</h1>
+                <div style={{color:"red"}}>
+                    {errorMessage}
+                </div>
+                <Link to={"/users"}>Back to users</Link>
+            </div>
+        )
+    } else {
+        return (
+            <div>
+                <h1>User</h1>
+                {user.user_id}: {user.username}
+                <Link to={"/users"}>Back to users</Link>
+                <button type={"button"}>
+                    Edit
+                </button>
+                <button type="button" className="btn btn-primary" data-bs-toggle="modal"
+                        data-bs-target="#deleteUserModal">
+                    Delete
+                </button>
+                <div className="modal fade" id="deleteUserModal" tabIndex={-1} role="dialog"
+                     aria-labelledby="deleteUserModalLabel" aria-hidden="true">
+                    <div className="modal-dialog">
+                        <div className="modal-content">
+                            <div className="modal-header">
+                                <h5 className="modal-title"
+                                    id="deleteUserModalLabel">Delete User</h5>
+                                <button type="button" className="btn-close" data-bs-dismiss="modal" aria-label="Close">
+                                </button>
+                            </div>
+                            <div className="modal-body">
+                                Are you sure that you want to delete this user?
+                            </div>
+                            <div className="modal-footer">
+                                <button type="button" className="btn btn-secondary" data-bs-dismiss="modal">
+                                    Close
+                                </button>
+                                <button type="button" className="btn btn-primary" data-bs-dismiss="modal"
+                                        onClick={() => deleteUser(user)}>
+                                    Delete User
+                                </button>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        )
+    }
+}
+export default User;
